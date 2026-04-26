d195a7e507ba5b26e836480f67b8eb1a1e327f59
NanaProject <nanaproject2023@yahoo.com>
2026-04-26 01:55:40 -0400

    na

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,11 +16,7 @@ export default defineConfig({
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production" &&
-    process.env.GITHUB_ACTIONS
-      ? "/introproject/"
-      : "/",
-});
+  base: mode === "github" ? "/introproject/" : "/",
+}));
